775b868546d29e0e65363578d23823287994f784
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-11-20 15:24:53 +0530

    Answering Questions

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,24 @@
 import "../App.css";
 import { Questions } from "../helpers/Questions";
+import { useState } from "react";
 
 function Quiz() {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [optionChosen, setOptionChosen] = useState("");
+
+    const chooseOption = (option) => {
+        setOptionChosen(option)
+    }
     return(
         <div className="Quiz">
-            {Questions[1].prompt}
+            <h1>{Questions[currentQuestion].prompt}</h1>
+            <div className="questions">
+                <button onClick={() => {chooseOption('optionA')}} >{Questions[currentQuestion].optionA} </button>
+                <button onClick={() => {chooseOption('optionB')}} >{Questions[currentQuestion].optionB} </button>
+                <button onClick={() => {chooseOption('optionC')}} >{Questions[currentQuestion].optionC} </button>
+                <button onClick={() => {chooseOption('optionD')}} >{Questions[currentQuestion].optionD} </button>
+            </div>
+            {optionChosen}
         </div>
     )
 }
