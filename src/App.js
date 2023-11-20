6f513f538385420fb12be51b78a9dbadab9069bd
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-11-20 11:26:49 +0530

    Setting Up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Menu from './components/Menu';
 function App() {
   return (
     <div className="App">
-        Hello
+        <h1>Quiz App</h1>
+        <Menu/>
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import "../App.css"
+
+function Menu() {
+    return (
+        <div className="Menu">
+        <input type="text" placeholder="Ex. John Smith" />
+            <button>Start Quiz</button>
+        </div>
+    )
+}
+
+export default Menu;
