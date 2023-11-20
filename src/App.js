62e0f72e318831dda942e43b43522220c9c61e49
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-11-20 11:43:58 +0530

    Create the Menu Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Menu from './components/Menu';
+import { useState } from 'react';
+
+// ['menu', 'playing', 'finished']
 function App() {
+  const [gameState, setGameState] = useState("menu");
   return (
     <div className="App">
         <h1>Quiz App</h1>
-        <Menu/>
+        {gameState === "menu" && <Menu/> }
+        
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,6 +3,7 @@ import "../App.css"
 function Menu() {
     return (
         <div className="Menu">
+        <label htmlFor="">Enter Your Name: </label>
         <input type="text" placeholder="Ex. John Smith" />
             <button>Start Quiz</button>
         </div>
