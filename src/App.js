fc93b0cee175e38a710260c5c957a91b15c9b29e
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-11-20 13:54:17 +0530

    Global States with the context API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Menu from './components/Menu';
 import { useState } from 'react';
+import {GameStateContext} from "./helpers/Context"
 
 // ['menu', 'playing', 'finished']
 function App() {
   const [gameState, setGameState] = useState("menu");
+
+
+
+
   return (
     <div className="App">
         <h1>Quiz App</h1>
+        <GameStateContext.Provider value={{gameState, setGameState}}>
         {gameState === "menu" && <Menu/> }
+        </GameStateContext.Provider>
         
     </div>
   );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,22 @@
-import "../App.css"
+import "../App.css";
+import { useContext } from "react";
+import { GameStateContext } from "../helpers/Context";
 
 function Menu() {
-    return (
-        <div className="Menu">
-        <label htmlFor="">Enter Your Name: </label>
-        <input type="text" placeholder="Ex. John Smith" />
-            <button>Start Quiz</button>
-        </div>
-    )
+  const { gameState, setGameState } = useContext(GameStateContext);
+  return (
+    <div className="Menu">
+      <label htmlFor="">Enter Your Name: </label>
+      <input type="text" placeholder="Ex. John Smith" />
+      <button
+        onClick={() => {
+          setGameState("Playing");
+        }}
+      >
+        Start Quiz
+      </button>
+    </div>
+  );
 }
 
 export default Menu;
